cdfb8bc99cfbaab6c449658fac948a110d22d3fb
Koboo <14143908+Koboo@users.noreply.github.com>
2022-09-26 09:56:17 +0200

    Create debug_checkfuel.lua

---
+++ debug_checkfuel.lua
@@ -0,0 +1,19 @@
+-- Refueling Pro v1 by Koboo
+function checkFuel() 
+    if turtle.getFuelLevel() < 5 then
+        if turtle.getItemCount(1) < 1 then
+            print("Waiting for fuel..")
+            while not turtle.refuel(0) do
+                turtle.select(1)
+                if turtle.getItemCount(1) > 0 then
+                    print("I can't use item in slot '1' as fuel!")
+                end
+                sleep(2.5)
+            end
+        end
+        turtle.refuel(1)
+        print("Mhh.. tasty fuel! (level="..tostring(turtle.getFuelLevel())..")")
+    end
+end
+ 
+checkFuel()
