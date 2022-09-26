3e457911ef1c2922c543541c48626b3860385dfc
Koboo <14143908+Koboo@users.noreply.github.com>
2022-09-26 09:55:01 +0200

    Create farm_resin.lua

---
+++ farm_resin.lua
@@ -0,0 +1,48 @@
+function checkFuel() 
+    if turtle.getFuelLevel() < 5 then
+        if turtle.getItemCount(1) < 1 then
+            print("Waiting for fuel in slot 1..")
+            while not turtle.refuel(0) do
+                turtle.select(1)
+                if turtle.getItemCount(1) > 0 then
+                    print("I can't use item in slot 1 as fuel!")
+                end
+                sleep(2.5)
+            end
+        end
+        turtle.refuel(1)
+        print("Mhh.. tasty fuel! (level="..tostring(turtle.getFuelLevel())..")")
+    end
+end
+ 
+function checkItem(itemName, slot)
+    local data = turtle.getItemDetail()
+    if turtle.getItemCount(slot) == 0 and not (data and data.name == itemName) then
+        print("Waiting for "..tostring(itemName).." in slot "..tostring(slot).."..")
+        turtle.select(slot)
+        while not (data and data.name == itemName) do
+            data = turtle.getItemDetail()
+            sleep(2.5)
+        end
+    end
+end
+ 
+function farmResin()
+    local woodName = "ic2:blockrubwood"
+    checkItem(woodName, 2)
+    local blocks = 0
+    while not turtle.detectDown() do
+        checkFuel()
+        turtle.select(2)
+        turtle.placeDown()
+        turtle.digDown()
+        blocks = blocks + 1
+        if blocks == 10 then
+            print("Farmed sticky resin! Ending script..")
+            break
+        end
+        sleep(1)
+    end
+end
+ 
+farmResin()
